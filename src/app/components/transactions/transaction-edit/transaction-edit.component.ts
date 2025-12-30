fa54cae8523459352e55804582e23b35b42321ac
Manoshri Chattopadhyay <manoshri.no@gmail.com>
2025-12-30 11:08:40 +0100

    refactor edit component

--- src/app/components/transactions/transaction-edit/transaction-edit.component.ts
+++ src/app/components/transactions/transaction-edit/transaction-edit.component.ts
@@ -1,60 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { Transaction } from '../../../models/transaction.model';
+import { Observable, of } from 'rxjs';
 import { Category } from '../../../models/category.model';
+import { Transaction, TransactionType } from '../../../models/transaction.model';
+import { FilterCategoryPipe } from "../../../pipes/filter-category.pipe";
+import { CategoryStoreService } from '../../../services/category-store.service';
+import { TransactionStoreService } from '../../../services/transaction-store.service';
 import { TransactionService } from '../../../services/transaction.service';
-import { CategoriesService } from '../../../services/categories.service';
-
 
 @Component({
   selector: 'app-transaction-edit',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule, JsonPipe, FilterCategoryPipe],
   templateUrl: './transaction-edit.component.html',
   styleUrls: ['./transaction-edit.component.scss']
 })
+
 export class TransactionEditComponent implements OnInit {
 
-  transaction!: Transaction;
-  categories: Category[] = [];
+  transactions$: Observable<Transaction[]> = of([]);
+  categories$: Observable<Category[]> = of([]);
+  selectedTransaction: Transaction | undefined;
+  updatedTransaction: Transaction | undefined;
 
-  constructor(
-    private route: ActivatedRoute,
-    private router: Router,
-    private transactionService: TransactionService,
-    private categoryService: CategoriesService
-  ) { }
+  private categoryStoreServices = inject(CategoryStoreService);
+  private transactionStoreServices = inject(TransactionStoreService);
+  private transactionService = inject(TransactionService);
+  private route = inject(ActivatedRoute);
+  private router = inject(Router);
+  private formBuilder = inject(FormBuilder);
+
+  editTransactionForm = this.formBuilder.nonNullable.group({
+    type: ['expense' as TransactionType, Validators.required],
+    title: ['', Validators.required],
+    amount: [0, [Validators.required, Validators.min(1)]],
+    categoryId: [''],
+    date: ['', Validators.required]
+  });
 
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (!id) return;
+    const editId = this.route.snapshot.paramMap.get('id');
+    if (!editId) return;
 
-    this.loadCategories();
+    this.categoryStoreServices.initCategory();
+    this.transactionStoreServices.initTransaction();
 
-    this.transactionService
-      .getTransactionById(id)
-      .subscribe(data => this.transaction = data);
+    this.transactions$ = this.transactionStoreServices.transactions$;
+    this.categories$ = this.categoryStoreServices.categories$;
+
+
+    this.transactions$.subscribe((transactions) => transactions.filter((transaction) => {
+      if (transaction.id === editId) {
+        this.selectedTransaction = transaction;
+        this.editTransactionForm.get('type')?.setValue(transaction.type);
+        this.editTransactionForm.get('title')?.setValue(transaction.title);
+        this.editTransactionForm.get('categoryId')?.setValue(transaction.categoryId);
+        this.editTransactionForm.get('amount')?.setValue(transaction.amount);
+        this.editTransactionForm.get('date')?.setValue(transaction.date);
+      }
+    }));
+
   }
-
-  loadCategories(): void {
-    this.categoryService
-      .getCategories()
-      .subscribe(data => this.categories = data);
-  }
-
 
   updateTransaction(): void {
-    this.transactionService.updateTransaction(this.transaction)
+    if (!this.selectedTransaction || this.editTransactionForm.invalid) {
+      return;
+    }
+
+    const updatedTransactionData: Transaction = {
+      ...this.selectedTransaction,
+      ...this.editTransactionForm.value
+    };
+
+    this.transactionService.updateTransaction(updatedTransactionData)
       .subscribe(() => {
         alert('Transaction updated successfully!');
+        this.transactionStoreServices.initTransaction(true)
         this.router.navigate(['/']);
       });
   }
 
 
-  cancel(): void {
+
+  cancelEdit(): void {
     this.router.navigate(['/']);
   }
 }
