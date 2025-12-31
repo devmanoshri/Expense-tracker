a04e1517dcec4b265ed5d35f2751faa8ed4b5590
Manoshri Chattopadhyay <manoshri.no@gmail.com>
2025-12-31 10:22:16 +0100

    update transaction pipe

--- src/app/pipes/filter-transaction.pipe.ts
+++ src/app/pipes/filter-transaction.pipe.ts
@@ -1,40 +1,46 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Transaction } from '../models/transaction.model';
 
+interface FilterOptions {
+  transactionType: string;
+  selectedCategoryId: string | null;
+  fromDate: string;
+  toDate: string;
+}
+
 @Pipe({
   name: 'filterTransaction',
-  standalone: true
+  standalone: true,
 })
 export class FilterTransactionPipe implements PipeTransform {
-
-  transform(transactions: Transaction[], selectedCategoryId: string | null, fromDate: string, toDate: string): Transaction[] {
-
-    if (!selectedCategoryId && !fromDate && !toDate) {
+  transform(
+    transactions: Transaction[],
+    filterOptions: FilterOptions
+  ): Transaction[] {
+    const { transactionType, selectedCategoryId, fromDate, toDate } =
+      filterOptions;
+    if (!transactionType && !selectedCategoryId && !fromDate && !toDate) {
       return transactions;
     }
 
+    if (selectedCategoryId === '') {
+      return transactions.filter(
+        (transaction) => transaction.type === transactionType
+      );
+    }
+
     return transactions.filter((transaction) => {
-
-      switch (selectedCategoryId) {
-        case 'income':
-          if (transaction.type !== 'income') { return false; }
-          break;
-        default:
-          if (transaction.type !== 'expense') { return false; }
-          if (transaction.categoryId !== selectedCategoryId) { return false; }
-          break;
-      }
-
       const transactionDate = new Date(transaction.date).getTime();
       const from = fromDate ? new Date(fromDate).getTime() : null;
       const to = toDate ? new Date(toDate).getTime() : null;
 
+      if (transaction.categoryId !== selectedCategoryId) {
+        return false;
+      }
       if (from && transactionDate < from) return false;
       if (to && transactionDate > to) return false;
 
       return true;
     });
-
   }
-
 }
