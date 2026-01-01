69d7003b0f2aa0cceabcadd5c531d50e6804d009
Manoshri Chattopadhyay <manoshri.no@gmail.com>
2026-01-01 13:19:14 +0100

    feat: update summary component due to connect with chart component

---
+++ src/app/components/pages/chart-preview/transaction-summary/transaction-summary.component.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { Transaction } from '../../../../models/transaction.model';
+
+@Component({
+  selector: 'app-transaction-summary',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './transaction-summary.component.html',
+  styleUrls: ['./transaction-summary.component.scss'],
+})
+export class TransactionSummaryComponent {
+  @Input() selectedTransactions: Transaction[] | undefined;
+
+  get totalIncome(): number | undefined {
+    if (!this.selectedTransactions) return;
+    return this.selectedTransactions
+      .filter((transaction) => transaction.type === 'income')
+      .reduce((sum, transaction) => sum + transaction.amount, 0);
+  }
+
+  get totalExpense(): number | undefined {
+    if (!this.selectedTransactions) return;
+    return this.selectedTransactions
+      .filter((transaction) => transaction.type === 'expense')
+      .reduce((sum, transaction) => sum + transaction.amount, 0);
+  }
+
+  get balance(): number | undefined {
+    if (!this.totalIncome || !this.totalExpense) return;
+    return this.totalIncome - this.totalExpense;
+  }
+}
