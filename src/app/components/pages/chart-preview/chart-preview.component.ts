7fd05f40d8d02474a4193f423f4201913b99e8e0
Manoshri Chattopadhyay <manoshri.no@gmail.com>
2026-01-01 13:15:46 +0100

    feat: add chart preview component due to update folder structure

---
+++ src/app/components/pages/chart-preview/chart-preview.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { TransactionChartComponent } from '../../shared/transaction-chart/transaction-chart.component';
+import { Transaction } from '../../../models/transaction.model';
+import { TransactionSummaryComponent } from "./transaction-summary/transaction-summary.component";
+
+@Component({
+  selector: 'app-chart-preview',
+  imports: [TransactionChartComponent, TransactionSummaryComponent],
+  templateUrl: './chart-preview.component.html',
+  styleUrl: './chart-preview.component.scss',
+})
+export class ChartPreviewComponent {
+  transactionsChangeFromChart: Transaction[] = [];
+
+  onTransactionsChange(transactions: Transaction[]) {
+    this.transactionsChangeFromChart = transactions;
+  }
+}
