f3da54101026df40ecedd924c8ed1f3c1e0f0021
Manoshri Chattopadhyay <manoshri.no@gmail.com>
2026-01-13 19:58:03 +0100

    fix: code format

--- src/app/resolvers/transactions.resolver.ts
+++ src/app/resolvers/transactions.resolver.ts
@@ -1,7 +1,6 @@
+import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
 import { Transaction } from '../models/transaction.model';
-import { TransactionStoreService } from '../services/transaction-store.service';
-import { inject } from '@angular/core';
 import { TransactionService } from '../services/transaction.service';
 
 export const transactionsResolver: ResolveFn<Transaction[]> = () => {
